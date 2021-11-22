de4cefa0df1d4bc6a9a7ee5961c06cf22f0f5701
stovet <steven.topalian@gmail.com>
2021-11-22 16:31:11 -0500

    added if incase of negative #

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function logRectInfo(topLeft, bottomRight){
-    let length = bottomRight[0]- topLeft[0];
+    let length = topLeft[0] - bottomRight[0]
+    if(length < 0){
+        length *= -1;
+    }
     console.log(`length: ${length}`);
     let width = topLeft[1] - bottomRight[1];
+    if(width < 0){
+        width *= -1;
+    }
     console.log(`width: ${width}`)
 
     console.log(`The perimeter is: ${2*(length + width)}`)
