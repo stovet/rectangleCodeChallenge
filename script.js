c94faa74cfecb260e9efa03fe2eff8480256d27c
stovet <steven.topalian@gmail.com>
2021-11-22 16:21:49 -0500

    specified length and width for log

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function logRectInfo(topLeft, bottomRight){
     let length = bottomRight[0]- topLeft[0];
-    console.log(length);
+    console.log(`length: ${length}`);
     let width = topLeft[1] - bottomRight[1];
-    console.log(width)
+    console.log(`width: ${width}`)
 
     console.log(`The perimeter is: ${2*(length + width)}`)
     console.log(`The area is: ${length * width}`)
